010bbbd222d531444666a957f9b6e7da4dacf104
nuxen <theonenuxen@proton.me>
2025-07-08 02:33:40 +0200

    Support for multiple seriesIds in Rename API endpoint

--- src/Sonarr.Api.V3/Episodes/RenameEpisodeController.cs
+++ src/Sonarr.Api.V3/Episodes/RenameEpisodeController.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.MediaFiles;
 using Sonarr.Http;
+using Sonarr.Http.REST;
 
 namespace Sonarr.Api.V3.Episodes
 {
@@ -25,6 +27,23 @@ namespace Sonarr.Api.V3.Episodes
             }
 
             return _renameEpisodeFileService.GetRenamePreviews(seriesId).ToResource();
+        }
+
+        [HttpGet("bulk")]
+        [Produces("application/json")]
+        public List<RenameEpisodeResource> GetEpisodes([FromQuery] List<int> seriesIds)
+        {
+            if (seriesIds is { Count: 0 })
+            {
+                throw new BadRequestException("seriesIds must be provided");
+            }
+
+            if (seriesIds.Any(seriesId => seriesId <= 0))
+            {
+                throw new BadRequestException("seriesIds must be positive integers");
+            }
+
+            return _renameEpisodeFileService.GetRenamePreviews(seriesIds).ToResource();
         }
     }
 }
