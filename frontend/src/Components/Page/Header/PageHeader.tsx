d84748007999fbbf37483e7db3831ecb1e5cee7a
vtorres <vtorres@>
2025-12-15 16:50:06 +0100

    fix

--- frontend/src/Components/Page/Header/PageHeader.tsx
+++ frontend/src/Components/Page/Header/PageHeader.tsx
@@ -4,7 +4,6 @@ import IconButton from 'Components/Link/IconButton';
 import Link from 'Components/Link/Link';
 import useKeyboardShortcuts from 'Helpers/Hooks/useKeyboardShortcuts';
 import { icons } from 'Helpers/Props';
-import translate from 'Utilities/String/translate';
 import KeyboardShortcutsModal from './KeyboardShortcutsModal';
 import PageHeaderActionsMenu from './PageHeaderActionsMenu';
 import SeriesSearchInput from './SeriesSearchInput';
